43ce57a7c25219be824590f4e642a034186e7cad
s2choco <kyj_0129@naver.com>
2020-03-30 14:52:58 +0900

    splash 화면 완성 & app logo 변경

--- app/src/main/java/com/foreknowledge/rssnewsreader/ui/SplashActivity.kt
+++ app/src/main/java/com/foreknowledge/rssnewsreader/ui/SplashActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.foreknowledge.rssnewsreader.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
