007d1a0bbad3ff890a2e1a447dad7a96a574bf48
s2choco <kyj_0129@naver.com>
2020-03-30 15:15:41 +0900

    News Item 중복 클릭 방지

--- app/src/main/java/com/foreknowledge/rssnewsreader/Constants.kt
+++ app/src/main/java/com/foreknowledge/rssnewsreader/Constants.kt
@@ -4,3 +4,4 @@ const val RSS_URL = "https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko"
 const val EXTRA_TITLE = "extra_title"
 const val EXTRA_LINK = "extra_link"
 const val EXTRA_KEYWORDS = "extra_keywords"
+const val MIN_CLICK_INTERVAL = 1000
